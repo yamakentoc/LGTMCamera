d56a31bd7da7e8ae61fbfa940f1ce266ca5836ca
yamakentoc <yamakentoc@gmail.com>
2019-01-11 19:23:30 +0900

    [feat] cellの大きさを調整

--- LGTMCamera/AfterShooting/AfterShootingScroll/AfterShootingScrollViewController.swift
+++ LGTMCamera/AfterShooting/AfterShootingScroll/AfterShootingScrollViewController.swift
@@ -33,30 +33,37 @@ extension AfterShootingScrollViewController: UITableViewDelegate, UITableViewDat
         cell?.selectionStyle = .none
         let horizontalScrollView: ASHorizontalScrollView = ASHorizontalScrollView(frame: self.tableView.frame)
         horizontalScrollView.arrangeType = .byNumber
-        horizontalScrollView.marginSettings_320 = MarginSettings(leftMargin: 10, numberOfItemsPerScreen: 4.25)
-        horizontalScrollView.marginSettings_480 = MarginSettings(leftMargin: 10, numberOfItemsPerScreen: 5.25)
-        horizontalScrollView.marginSettings_414 = MarginSettings(leftMargin: 10, numberOfItemsPerScreen: 4.25)
-        horizontalScrollView.marginSettings_736 = MarginSettings(leftMargin: 10, numberOfItemsPerScreen: 7.375)
-        horizontalScrollView.uniformItemSize = CGSize(width: 80, height: 50)
+        //leftMargin: 左から何ポイント離すか numberOf~: 画面に初期で何個表示するか
+        horizontalScrollView.defaultMarginSettings = MarginSettings(leftMargin: 10,
+                                                                    numberOfItemsPerScreen: 4)
+        let squareSize = self.tableView.bounds.height * 0.3
+        horizontalScrollView.uniformItemSize = CGSize(width: squareSize, height: squareSize)
         horizontalScrollView.setItemsMarginOnce()
-        for _ in 1...20 {
+        for _ in 1...6 {
             let button = UIButton(frame: CGRect.zero)
             button.backgroundColor = UIColor.red
             horizontalScrollView.addItem(button)
         }
         cell?.contentView.addSubview(horizontalScrollView)
         horizontalScrollView.translatesAutoresizingMaskIntoConstraints = false
-        cell?.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: cell!.contentView, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1, constant: 0))
-        cell?.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: cell!.contentView, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1, constant: 0))
-        cell?.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.tableView.bounds.height))
-        cell?.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: cell!.contentView, attribute: NSLayoutConstraint.Attribute.width, multiplier: 1, constant: 0))
+        cell?.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.left,
+                                                           relatedBy: NSLayoutConstraint.Relation.equal, toItem: cell!.contentView,
+                                                           attribute: NSLayoutConstraint.Attribute.left, multiplier: 1, constant: 0))
+        cell?.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.top,
+                                                           relatedBy: NSLayoutConstraint.Relation.equal, toItem: cell!.contentView,
+                                                           attribute: NSLayoutConstraint.Attribute.top, multiplier: 1, constant: 0))
+        cell?.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.height,
+                                                           relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil,
+                                                           attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.tableView.bounds.height))
+        cell?.contentView.addConstraint(NSLayoutConstraint(item: horizontalScrollView, attribute: NSLayoutConstraint.Attribute.width,
+                                                           relatedBy: NSLayoutConstraint.Relation.equal, toItem: cell!.contentView,
+                                                           attribute: NSLayoutConstraint.Attribute.width, multiplier: 1, constant: 0))
         return cell!
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return self.tableView.bounds.height
     }
-    
 }
 
 extension AfterShootingScrollViewController: AfterShootingScrollViewPresenterProtocol {
