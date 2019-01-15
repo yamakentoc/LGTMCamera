ebac7ca5b5455f31fcf41e3e6c7f2dbbd44ce34b
yamakentoc <yamakentoc@gmail.com>
2019-01-15 17:34:41 +0900

    [feat] saveButtonをbottomMenuに

--- LGTMCamera/AfterShooting/AfterShootingViewController.swift
+++ LGTMCamera/AfterShooting/AfterShootingViewController.swift
@@ -8,10 +8,7 @@
 
 import UIKit
 import AVFoundation
-import ImageIO
-import MobileCoreServices
 import Gifu
-import SVProgressHUD
 import Photos
 
 protocol AfterShootingDelegate: class {
@@ -23,9 +20,7 @@ class AfterShootingViewController: UIViewController {
     @IBOutlet weak var gifImageView: UIImageView!
     @IBOutlet weak var bottomMenuView: UIView!
     @IBOutlet weak var adjustSpeedView: UIView!
-    
-    @IBOutlet weak var saveButton: UIButton!
-    
+        
     var presenter: AfterShootingViewPresenter!
     weak var delegate: AfterShootingDelegate?
     var containerViews: [UIView] = []
@@ -43,21 +38,11 @@ class AfterShootingViewController: UIViewController {
         activeTimer()
     }
     
-    @IBAction func tapSaveButton(_ sender: UIButton) {
-        makeGifImage()
-    }
-    
     @IBAction func tapBackButton(_ sender: UIButton) {
         timer?.invalidate()
         self.delegate?.resizeButton()
         self.dismiss(animated: true, completion: nil)
     }
-    
-//    @IBAction func adjustGifSpeed(_ sender: UISlider) {//画像の切り替えspeedを調整
-//        timer?.invalidate()
-//        frameRate = CMTimeMake(value: 1, timescale: Int32(sender.value))
-//        activeTimer()
-//    }
     
     func activeTimer() {//timerの生成
         let cmTime = CMTimeGetSeconds(frameRate)
@@ -73,45 +58,6 @@ class AfterShootingViewController: UIViewController {
     
     func switchBottomView(tag: Int) {
         self.view.bringSubviewToFront(containerViews[tag])
-    }
-    
-    //gifを保存
-    func makeGifImage() {
-        SVProgressHUD.show(withStatus: "saving gif")
-        let fileProperties = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFLoopCount as String: 0]]//ループカウント
-        let frameProperties = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFDelayTime as String: CMTimeGetSeconds(self.frameRate)]]//フレームレート
-        let url = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("\(NSUUID().uuidString).gif")
-        guard let destination = CGImageDestinationCreateWithURL(url! as CFURL, kUTTypeGIF, self.takenPhotos.count, nil) else { return } //保存先
-        CGImageDestinationSetProperties(destination, fileProperties as CFDictionary?)
-        DispatchQueue.global(qos: .default).async {
-            for image in self.takenPhotos {
-                guard let translatedImage = self.translate(image) else { return }
-                CGImageDestinationAddImage(destination, translatedImage, frameProperties as CFDictionary)
-            }
-            if CGImageDestinationFinalize(destination) {//GIF生成後の処理
-                DispatchQueue.main.async {
-                    PHPhotoLibrary.shared().performChanges({//gifを保存
-                        PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL: url!)
-                    }, completionHandler: { (_, _) in
-                        SVProgressHUD.setMinimumDismissTimeInterval(1.0)
-                        SVProgressHUD.showSuccess(withStatus: "saved!")
-                    })
-                }
-            } else {
-                SVProgressHUD.showError(withStatus: "error!")
-            }
-        }
-    }
-    
-    //UIImageからCGImageにそのまま変換すると画像の向きが保持されないから
-    func translate(_ image: UIImage) -> CGImage? {
-        guard let cgImage = image.cgImage else { return nil }
-        ////元画像から右に90度回転(内カメラの時は反転状態に)
-        let orientation: CGImagePropertyOrientation = self.isBackCamera ? .right : .leftMirrored
-        let ciImage = CIImage(cgImage: cgImage).oriented(orientation)
-        let ciContext = CIContext(options: nil)
-        guard let cgImageFromCIImage: CGImage = ciContext.createCGImage(ciImage, from: ciImage.extent) else { return nil}
-        return cgImageFromCIImage
     }
 }
 
--- LGTMCamera/AfterShooting/BottomMenu/BottomMenuViewController.swift
+++ LGTMCamera/AfterShooting/BottomMenu/BottomMenuViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 import ASHorizontalScrollView
+import SVProgressHUD
+import Photos
+import ImageIO
+import MobileCoreServices
 
 class BottomMenuViewController: UIViewController {
 
@@ -20,6 +24,45 @@ class BottomMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter = BottomMenuViewPresenter(view: self)
+    }
+    
+    @IBAction func tapSaveButton() {
+        guard let afterShootingVC = self.parent as? AfterShootingViewController else { return }
+        SVProgressHUD.show(withStatus: "saving gif")
+        let fileProperties = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFLoopCount as String: 0]]//ループカウント
+        let frameProperties = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFDelayTime as String: CMTimeGetSeconds(afterShootingVC.frameRate)]]//フレームレート
+        let url = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("\(NSUUID().uuidString).gif")
+        guard let destination = CGImageDestinationCreateWithURL(url! as CFURL, kUTTypeGIF, afterShootingVC.takenPhotos.count, nil) else { return } //保存先
+        CGImageDestinationSetProperties(destination, fileProperties as CFDictionary?)
+        DispatchQueue.global(qos: .default).async {
+            for image in afterShootingVC.takenPhotos {
+                guard let translatedImage = self.translate(image) else { return }
+                CGImageDestinationAddImage(destination, translatedImage, frameProperties as CFDictionary)
+            }
+            if CGImageDestinationFinalize(destination) {//GIF生成後の処理
+                DispatchQueue.main.async {
+                    PHPhotoLibrary.shared().performChanges({//gifを保存
+                        PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL: url!)
+                    }, completionHandler: { (_, _) in
+                        SVProgressHUD.setMinimumDismissTimeInterval(1.0)
+                        SVProgressHUD.showSuccess(withStatus: "saved!")
+                    })
+                }
+            } else {
+                SVProgressHUD.showError(withStatus: "error!")
+            }
+        }
+    }
+    
+    //UIImageからCGImageにそのまま変換すると画像の向きが保持されないから
+    func translate(_ image: UIImage) -> CGImage? {
+        guard let cgImage = image.cgImage, let afterShootingVC = self.parent as? AfterShootingViewController else { return nil }
+        ////元画像から右に90度回転(内カメラの時は反転状態に)
+        let orientation: CGImagePropertyOrientation = afterShootingVC.isBackCamera ? .right : .leftMirrored
+        let ciImage = CIImage(cgImage: cgImage).oriented(orientation)
+        let ciContext = CIContext(options: nil)
+        guard let cgImageFromCIImage: CGImage = ciContext.createCGImage(ciImage, from: ciImage.extent) else { return nil}
+        return cgImageFromCIImage
     }
     
     @objc func selectBottomMenu(_ sender: UIButton) {
